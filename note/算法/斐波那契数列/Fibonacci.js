21639eeab87daec92fc828c05a5f71cd9fcc6160
nzq <1948389397@qq.com>
2019-03-04 16:14:49 +0800

    函数柯里化 尾调用优化

---
+++ note/算法/斐波那契数列/Fibonacci.js
@@ -0,0 +1,41 @@
+// 其实就是一个 num2 = num1 + num2(原来值)  和  num1 = num2(原来值) 的一个过程
+// F(1)=1，F(2)=1, F(n)=F(n-1)+F(n-2)（n>=3，n∈N*）
+
+'use strict';
+
+function fibonacci(n) {
+  var num1 = 1;
+  var num2 = 1;
+  var count = 0;
+
+  if (n < 3) {
+    return 1;
+  }
+  for (; n>=3; n--) {
+    [num2, num1] = [num1 + num2, num2]
+  }
+  return num2;
+}
+
+
+// 非尾递归， 栈溢出
+function fibonacci1(n) {
+  if (n <= 2) {
+    return 1;
+  }
+
+  return fibonacci1(n - 1) + fibonacci1(n - 2)
+}
+
+
+// 尾递归， 栈溢出
+function fibonacci2(n, num1 = 1, num2 = 1) {
+  if (n <= 2) {
+    return num2;
+  }
+
+  return fibonacci2(n-1, num2, num2 + num1)
+}
+
+
+console.log(fibonacci2(100000));
