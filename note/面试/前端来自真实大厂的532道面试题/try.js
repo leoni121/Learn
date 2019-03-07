92fb73bb055fb03d0bb3f7c1199dfacb6b784748
nzq <1948389397@qq.com>
2019-03-08 00:57:22 +0800

    nzq

--- note/面试/前端来自真实大厂的532道面试题/try.js
+++ note/面试/前端来自真实大厂的532道面试题/try.js
@@ -1,24 +1,11 @@
-function get (str) {
-  let len = str.length;
-  let maxStr = "";
-  let start = 0;
-  let end = 0;
-  str = str.split("").join("_");
 
-  for(let i = 0 ; i < len ; i++ ) {
-    start = i - 1;
-    end = i + 1;
-    while(start >= 0 && end < len && str[start] === str[end]) {
-      start -- ;
-      end ++;
-    }
-    if(end - start + 1 > maxStr.length) {
-      maxStr = str.substring(start, end + 1);
-    }
-  }
+var object = {
+  b: { c: 4 }, d: [{ e: 5 }, { e: 6 }]
+};
+console.log( parse(object, 'b.c') === 4 ) //true
+console.log( parse(object, 'd[0].e') === 5 ) //true
+console.log( parse(object, 'd.0.e') === 5 ) //true
+console.log( parse(object, 'd[1].e') === 6 ) //true
+console.log( parse(object, 'd.1.e') === 6 ) //true
+console.log( parse(object, 'f') === 'undefined' ) //true
 
-  return maxStr.split("_").join("");
-}
-
-
-console.log(get("1231231231"))
