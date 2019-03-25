3006e6641c229f8f26dc500085e97ba62a6bbf2f
nzq <1948389397@qq.com>
2019-03-26 00:39:41 +0800

    nzq

--- note/面试/前端来自真实大厂的532道面试题/实现eval.js
+++ note/面试/前端来自真实大厂的532道面试题/实现eval.js
@@ -8,21 +8,4 @@ const OBJ = {
  * @Param:
  * @Return:
  */
-function _eval(str) {
-  let arr = str.split(".");
-  let len = arr.length;
-  let temp = null;
-  if (arr[0] === "obj") {
-    temp = OBJ;
-    for (let i = 1; i < len; i++) {
-      if (temp[arr[i]]) {
-        temp = temp[arr[i]];
-      } else {
-        return undefined
-      }
-    }
-  }
-  return temp;
-}
-
-console.log(_eval("obj.name"))
+console.log("nzq")
