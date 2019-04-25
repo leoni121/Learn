d7d885a50077e84c61ae81db8633e30bb3cfba47
nzq <1948389397@qq.com>
2019-04-26 00:31:05 +0800

    continue vue-源码 明天开始

--- note/算法/剑指offer/4. 重建二叉树.js
+++ note/算法/剑指offer/4. 重建二叉树.js
@@ -5,12 +5,42 @@
  * @Param:
  * @Return:
  */
+const util = require('util');
+/*class Node {
+  constructor ({left=null, right=null, val=null}) {
+    this.left = left;
+    this.right = right;
+    this.val = val;
+  }
+}*/
+function TreeNode(x=0) {
+  this.val = x;
+  this.left = null;
+  this.right = null;
+}
 
-      1
-    2
-  4
-    7
+function reConstructBinaryTree(pre, vin) {
+  let vinLTreeArr = [],
+    vinRTreeArr = [],
+    preLTreeArr = [],
+    preRTreeArr = [],
+    node = null,
+    rootVal = (pre && pre[0]) && pre[0];
 
-      1
-    2   4
-          7
+  if (rootVal) {
+    node = new TreeNode();
+    let temIdx = vin.indexOf(rootVal);
+    vinLTreeArr = vin.slice(0, temIdx);
+    vinRTreeArr = vin.slice(temIdx+1);
+    preLTreeArr = pre[0] ? pre.slice(1, vinLTreeArr.length + 1) : [];
+    preRTreeArr = pre[0] ? pre.slice(vinLTreeArr.length+1) : [];
+
+    node.val = rootVal;
+    node.left = reConstructBinaryTree(preLTreeArr, vinLTreeArr);
+    node.right = reConstructBinaryTree(preRTreeArr, vinRTreeArr);
+  }
+
+  return node;
+}
+
+console.log(util.inspect(reConstructBinaryTree([1,2,4,7,3,5,6,8], [4,7,2,1,5,3,8,6]), true, 10))
