3603da7c6eaddcdeccc1c3c90c92e4854dbac8f1
1948389397@qq.com <1948389397@qq.com>
2019-06-24 08:40:36 +0800

    continue

--- note/React/learn/my-app/src/App.js
+++ note/React/learn/my-app/src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
-import {
-  ShoppingList,
-  i
-} from './shoppingList.js';
 import './App.css';
+
+var myStyle = {
+  color: 'red',
+  textAlign: 'center'
+}
+
+let arr = [
+  <div> nzq </div>,
+  <div> wx </div>
+]
+
+class Name extends React.Component {
+  render() {
+    return <h1 style={myStyle}>网站名称：{arr}</h1>;
+  }
+}
+class Url extends React.Component {
+  render() {
+    return <h1>网站地址：{this.props.url}</h1>;
+  }
+}
+class Nickname extends React.Component {
+  render() {
+    return <h1>网站地址：{this.props.nickname}</h1>;
+  }
+}
 
 function App() {
   return (
     <div>
-      <ShoppingList name='nzq'/>
+      <Name/>
+      <Url/>
+      <Nickname/>
     </div>
   )
 }
